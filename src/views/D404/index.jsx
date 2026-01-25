67fd513b9df863089e19f213bab90a31213af956
Rennie Benoit <benoit.ren01@outlook.fr>
2026-01-25 14:55:21 +0100

    error-page finished

--- src/views/D404/index.jsx
+++ src/views/D404/index.jsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom";
 
 export const D404 = () => {
   return (
-    <>
+    <section className="error-page">
       <h1>404</h1>
-      <p>Page introuvable</p>
-      <Link to="/">Retour à l’accueil</Link>
-    </>
+
+      <p>Oups ! La page que vous demandez n'existe pas.</p>
+
+      <Link to="/">Retourner sur la page d'accueil</Link>
+    </section>
   );
-}
+};
